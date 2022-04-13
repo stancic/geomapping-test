30a6c340f65f762f3a6cf7ce38007075676b3809
Dino <dino@prototyp.digital>
2022-04-13 20:32:07 +0200

    Example of polygon edit

--- modules/map/components/Region.tsx
+++ modules/map/components/Region.tsx
@@ -8,8 +8,13 @@ interface Props {
 export const Region: React.FC<Props> = ({ mapData }) => {
   if (!mapData) return null;
 
+  console.log('region', mapData);
+
   return (
-    <GeoJSON data={mapData.region}>
+    <GeoJSON
+      data={mapData.region}
+      style={{ fillColor: '#fff', color: '#ff3e3e', fillOpacity: 100 }}
+    >
       <Popup>{mapData.name}</Popup>
     </GeoJSON>
   );
